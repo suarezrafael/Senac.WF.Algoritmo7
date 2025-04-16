7589432b224085634558b342a580e47b2b5097c6
Rafael Suarez <rafaelv_s@hotmail.com>
2025-04-15 21:32:07 -0300

    login pronto

--- WF.Login/WF.Login/Form1.Designer.cs
+++ WF.Login/WF.Login/Form1.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 417);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(231, 256);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Black;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Calibri Light", 20F);
+            textBox1.ForeColor = SystemColors.ControlLight;
+            textBox1.Location = new Point(369, 123);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(351, 33);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "USUÁRIO";
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Calibri", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.Location = new Point(290, 371);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(157, 24);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Recuperar senha!";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Calibri Light", 20F);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(366, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(351, 41);
+            button1.TabIndex = 3;
+            button1.Text = "ENTRAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Black;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Calibri Light", 20F);
+            textBox2.ForeColor = SystemColors.ControlLight;
+            textBox2.Location = new Point(369, 177);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(351, 33);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "SENHA";
+            textBox2.Enter += textBox2_Enter;
+            textBox2.Leave += textBox2_Leave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ControlLight;
+            label1.Location = new Point(369, 146);
+            label1.Name = "label1";
+            label1.Size = new Size(327, 15);
+            label1.TabIndex = 6;
+            label1.Text = "________________________________________________________________";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ControlLight;
+            label2.Location = new Point(366, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(327, 15);
+            label2.TabIndex = 7;
+            label2.Text = "________________________________________________________________";
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonShadow;
+            button2.Location = new Point(724, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(47, 34);
+            button2.TabIndex = 8;
+            button2.Text = "X";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(783, 417);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(linkLabel1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.85D;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button button2;
     }
 }
--- WF.Login/WF.Login/Form1.cs
+++ WF.Login/WF.Login/Form1.cs
@@ -6,5 +6,47 @@ namespace WF.Login
         {
             InitializeComponent();
         }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "USUÁRIO")
+                textBox1.Text = string.Empty;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                textBox1.Text = "USUÁRIO";
+            }
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "SENHA")
+                textBox2.Text = string.Empty;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == string.Empty)
+            {
+                textBox2.Text = "SENHA";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var resposta = MessageBox.Show("Deseja encerrar o programa?",
+                "Sair",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(resposta == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+        }
     }
 }
